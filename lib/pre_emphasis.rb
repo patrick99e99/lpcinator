2ff5440e29170a4204655b690bd74e72b9302e5e
Patrick Collins <patrick@collinatorstudios.com>
2015-01-09 16:03:20 -0800

    fixes preemphasis filter normalization

--- lib/pre_emphasis.rb
+++ lib/pre_emphasis.rb
@@ -1,33 +1,32 @@
 module LPCinator
   class PreEmphasis < Bufferable
+    A = 0.5
+
     def self.process!(buffer)
       new(buffer).process!
     end
 
     def process!
-      max_level = 0
-      sum       = 0
+      max_level = 0;
+      sum       = 0;
 
       number_of_samples.times do |t|
         next if t.zero?
 
-        buffer[t] += buffer[t - 1] * multiplier
-        max_level = buffer[t].abs if buffer[t].abs > max_level
-        sum += buffer[t]
+        buffer[t] += buffer[t - 1] * A
+        absolute_buffer_value = buffer[t].abs
+        max_level = absolute_buffer_value if absolute_buffer_value > max_level
+        sum += buffer[t];
       end
 
       mean = sum / number_of_samples
+      max_level -= mean
       normalize!(max_level, mean)
-      buffer
-    end
-
-    def multiplier
-      0.5
     end
 
     def normalize!(max_level, mean)
       number_of_samples.times do |t|
-        buffer[t] = (buffer[t] - mean) / max_level
+        buffer[t] = (buffer[t] - mean) / max_level;
       end
     end
   end
